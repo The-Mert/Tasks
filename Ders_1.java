bde609c164198114fe68fd3d287bdd6b66920560
Melkor <myazicitoki@hotmail.com>
2023-10-25 15:22:30 +0300

    Ödev_1

---
+++ Ders_1.java
@@ -0,0 +1,73 @@
+package com.example.secondwithjava.odev1;
+
+public class Ders_1 {
+    public static void main(String[] args) {
+
+        String ilce = "Nilüfer";
+        String kita = "Asya";
+        String faks = "+90 (850) 885 2865";
+        String instaAdresi = "@dmertyq";
+        String calistiginBolum = "Android";
+        String musteriSoyadi = "Kara";
+        String dogumTarihi = "11.02.1982";
+        String medeniHali = "Bekar";
+        String videoYorumu = "Güzel içerik.";
+        String odemeSaati = "20:09";
+        String telefonModeli = "Samsung";
+        String dergiAdi = "National Geographic";
+        String basimTarihi = "16.08.2022";
+        String enlem = "15°N";
+        String boylam = "30°W";
+        String yemekAdi = "Kebap";
+        String sirket = "Yemek sepeti";
+        String videoAdi = "Lone Digger";
+        String muzikSuresi = "5 dakika";
+        String dosyaAdi = "Ders_1";
+        String resimFormati = "jpg";
+        String renk = "Koyu Mavi";
+        String renkKodu = "#1d1d86";
+        String bilgisayarModeli = "Acer";
+        String ekranBoyutu = "15 inç";
+        String etkinlikGunu = "Çarşamba";
+        String odemeGunu = "Cuma";
+        String yolculukCikisTarihi = "23.10.2023";
+        String mahalleAdi = "Mehmet Akif Ersoy";
+        String otobusHatti = "Ankara-Eskişehir-Bursa";
+        String kargoTakipNumarasi = "1A9KLct384";
+        String kuponKodu = "l9a2AtH";
+        String faturaTarihi = "30.09.2023";
+
+        final int postaKodu = 16130;
+        final int daireSayisi = 48;
+        int kullanimSuresi = 72;
+        int kullanilanDakika = 58;
+        int kuponSuresi = 12;
+        float basinc = 1.6f;
+        float zamMiktari = 3050.012f;
+        int mekanPuani = 7;
+        double urunFiyati = 7080.99;
+        double odemeMiktari = 7080;
+        int satilanMiktar = 352;
+        int eftMiktari = 2082;
+        int borc= 2082;
+        int urunMiktari = 712;
+
+        System.out.println(musteriSoyadi + " soydındaki müşterinin fatura adresi " + kita + "/" + ilce + ".");
+        System.out.println("Müşteri iletişim bilgileri: \n" + "-Faks: " + faks + ".\n-Posta kodu: " + postaKodu + ".\n-İnstagram adresi: " + instaAdresi + ".");
+        System.out.println("\nÖdeme Bilgileri: " + "\n-Bilgisayar modeli: " + bilgisayarModeli + ".\nÜrün fiyatı: " + urunFiyati + "TL.\n-Ödeme miktarı:" + odemeMiktari + "TL.\nEkran boyutu: " + ekranBoyutu + "." );
+        System.out.println("Çalıştığın Bölüm: " + calistiginBolum);
+        System.out.println("Kullanıcının medeni hali: " + medeniHali + "\nDoğum tarihi: " + dogumTarihi + ".");
+        System.out.println("Video adı: " + videoAdi + "\nKullanıcı yorumu: " + videoYorumu + "\nMüzik süresi: " + muzikSuresi + ".\nKullanılan dakika: " + kullanilanDakika + ".");
+        System.out.println("\nTelefon modeli: " + telefonModeli + "\n-Stok Miktarı: " + urunMiktari + ".\n-ürün fiyatı: " + urunFiyati+zamMiktari + ".");
+        System.out.println("\nMahalle adı: " + mahalleAdi + ".\nDaire sayısı: " + daireSayisi + ".\nSatılan mahalledeki toplam daire sayısı: " + satilanMiktar + ".");
+        System.out.println("\nSon fatura tarihi: " + faturaTarihi + "Ödeme günü: " + odemeGunu + ".\nÖdeme saati: " + odemeSaati + ".\nKullanım süresi: " + kullanimSuresi + ".");
+        System.out.println("\nDosya adı: " + dosyaAdi + ".\nResim foramtı: " + resimFormati + ".\nRenk: " + renk + ".\nRenk kodu: " + renkKodu + ".");
+        System.out.println("\nDergi adı: " + dergiAdi + "  Dergi basım tarihi: " + basimTarihi + ".");
+        System.out.println("\nŞirket ismi: " + sirket + ".\nYemek adı: " + yemekAdi + ".\nİndirim kuponu kodu: " + kuponKodu + ".\nİndirim kuponu süresi: " + kuponSuresi + " gün.");
+        System.out.println("\nEtkinlik günü: " + etkinlikGunu + ".\nEtkinlik konum: " + "   Enlem:" + enlem + ".     Boylam: " + boylam + ".\nEtkinlik mekanı puanı: " + mekanPuani + ".\nYola çıkış tarihi: " + yolculukCikisTarihi + "\nOtobüs hattı: " + otobusHatti);
+        System.out.println("\nToplam borç: " + borc + "TL.\nEft miktarı: " + eftMiktari + ".");
+        System.out.println("\nKargo takip numarası: " + kargoTakipNumarasi + ".");
+        System.out.println("\nKazan basıncı: " + basinc + " bar.");
+
+    }
+}
