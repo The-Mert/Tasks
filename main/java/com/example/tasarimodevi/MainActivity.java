a31e7b39a6c59635b3117933abc92a38b6c2cbd4
Mert <114438123+The-Mert@users.noreply.github.com>
2023-11-04 17:00:53 +0300

    Add files via upload
    Odev-3 (Tasarım)

---
+++ main/java/com/example/tasarimodevi/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.tasarimodevi;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.WindowCompat;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
